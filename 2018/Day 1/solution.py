383d204f7ff54da0333ab36d9081fcc38c1a5f57
gento <41165204+dscpsyl@users.noreply.github.com>
2022-12-09 12:28:09 -0800

    failed attempt

--- 2018/Day 1/solution.py
+++ 2018/Day 1/solution.py
@@ -1,5 +1,7 @@
 # https://adventofcode.com/2018
 
+import sys
+
 #? Part 1
 # with open("data.txt", 'r') as f:
 #     lines = [line.strip() for line in f]
@@ -17,6 +19,31 @@
 with open("data.txt", 'r') as f:
     lines = [line.strip() for line in f]
 
+freq = 0
 
+breaked = False
 
-print()
+lines = [eval(i) for i in lines]
+ 
+record = [0]
+
+while True:
+    for l in lines:
+        print(f"Current freq {freq}, change of {l}, ", end="")
+        freq += l
+        print(f"new freq {freq}, ", end="")
+        
+        for q in record:
+            if freq == q:
+                print("----------------------------------------------------------, ", freq)
+                breaked = True
+                sys.exit()
+        
+        if breaked:
+            break
+
+        record.append(freq)
+        print(f"records has length {len(record)}") 
+    
+    if breaked:
+        break
