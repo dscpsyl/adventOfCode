c1f29d6a4732c60fc768e1f83dcb388e3b6ae998
gento <41165204+dscpsyl@users.noreply.github.com>
2022-12-08 18:41:45 -0800

    imcomplete

---
+++ 2018/Day 1/solution.py
@@ -0,0 +1,22 @@
+# https://adventofcode.com/2018
+
+#? Part 1
+# with open("data.txt", 'r') as f:
+#     lines = [line.strip() for line in f]
+
+# freq = 0
+
+# lines = [eval(i) for i in lines]
+
+# for l in lines:
+#     freq += l
+
+# print(freq)
+    
+#? Part 2
+with open("data.txt", 'r') as f:
+    lines = [line.strip() for line in f]
+
+
+
+print()
