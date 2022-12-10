05619765ecb9e2f59e9978a2f0ea91e3c125f839
gento <41165204+dscpsyl@users.noreply.github.com>
2022-12-10 01:14:30 -0800

    2022/08

--- 2022/Day 8/solution.py
+++ 2022/Day 8/solution.py
@@ -1,15 +1,126 @@
+
+#? Part 1
+# with open("data.txt", 'r') as f:
+#     lines = [line.strip() for line in f]
+
+# for index, line in enumerate(lines):
+#     lines[index] = [*line]
+
+# edgeTrees = (2*len(lines[0])) + (2*(len(lines)-2))
+# innerTrees = 0
+
+# for i in range(1, len(lines)-1):
+#     for j in range(1, len(lines[i])-1):
+#         curr = lines[i][j]
+        
+#         # check up (constant j)
+#         k = i-1
+#         while k >= 0:
+#             if not curr > lines[k][j]:
+#                 break
+#             k -= 1
+#         else:
+#             innerTrees += 1
+#             continue
+        
+#         # check down (constant j)
+#         k = i+1
+#         while k < len(lines):
+#             if not curr > lines[k][j]:
+#                 break
+#             k += 1
+#         else:
+#             innerTrees += 1
+#             continue
+    
+#         # check left (constant i)
+#         l = j-1
+#         while l >= 0:
+#             if not curr > lines[i][l]:
+#                 break
+#             l -= 1
+#         else:
+#             innerTrees += 1
+#             continue
+    
+#         # check right  (constant i)
+#         l = j+1
+#         while l < len(lines):
+#             if not curr > lines[i][l]:
+#                 break
+#             l += 1
+#         else:
+#             innerTrees += 1
+#             continue
+
+# answer = innerTrees + edgeTrees
+# print(answer)
+
+#? Part 2
 with open("data.txt", 'r') as f:
     lines = [line.strip() for line in f]
 
 for index, line in enumerate(lines):
     lines[index] = [*line]
 
-edgeTrees = (2*len(lines[0])) + (2*(len(lines)-2))
-innerTrees = 0
+highestScore = 0
 
-
-
+for i in range(1, len(lines)-1):
+    for j in range(1, len(lines[i])-1):
+        curr = lines[i][j]
+        up = 1
+        down = 1
+        left = 1
+        right = 1
+        
+        # check up (constant j)
+        k = i-1
+        while k >= 0:
+            if curr > lines[k][j]:
+                up += 1
+            else:
+                break
+            k -= 1
+        else:
+            up -= 1
+        
+        # check down (constant j)
+        k = i+1
+        while k < len(lines):
+            if curr > lines[k][j]:
+                down += 1
+            else:
+                break
+            k += 1
+        else:
+            down -= 1
     
+        # check left (constant i)
+        l = j-1
+        while l >= 0:
+            if curr > lines[i][l]:
+                left += 1
+            else:
+                break
+            l -= 1
+        else:
+            left -= 1
+    
+        # check right  (constant i)
+        l = j+1
+        while l < len(lines):
+            if curr > lines[i][l]:
+                right += 1
+            else:
+                break
+            l += 1
+        else:
+            right -= 1
+        
+        # compare viewing score
+        score = up * down * left * right
+        if score > highestScore:
+            highestScore = score
+            print(f"Checking {i+1}, {j+1}, value {curr} | Up score {up} | Down score {down} | left score {left} | right score {right} | Final score {score}", end = "\n")
 
-answer = innerTrees + edgeTrees
-print(answer)
+print(highestScore)
