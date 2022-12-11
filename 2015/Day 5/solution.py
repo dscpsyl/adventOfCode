2cddeca160f042526569d50f9a894e96de2a0d20
gento <41165204+dscpsyl@users.noreply.github.com>
2022-12-11 00:54:59 -0800

    partial for 2015/05

---
+++ 2015/Day 5/solution.py
@@ -0,0 +1,31 @@
+
+#? Part 1
+with open("data.txt", 'r') as f:
+    lines = [line.strip() for line in f]
+    
+niceStrings = 0
+
+for l in lines:    
+    #Check for condition 1
+    # It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+    if True:
+        continue
+    else:
+        continue
+    
+    #Check for condition 2
+    # It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou
+    if True:
+        continue
+    else:
+        continue
+    
+    #Check for condition 3
+    #  It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+    if True:
+        continue
+    else:
+        continue
+    
+    
+    niceStrings += 1
